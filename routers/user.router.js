47c779dfe31a0df188cc572940026b7ff7ba80e8
mohit kushwaha <kushwahamohit000@gmail.com>
2021-08-20 18:01:04 +0530

    feat: save message route

--- routers/user.router.js
+++ routers/user.router.js
@@ -7,11 +7,15 @@ const {
   getById,
   findUser,
   deleteUser,
+  deleteSavedMessage,
+  saveMessage,
 } = require("../controllers/user.controller");
 
 router.route("/login").post(login);
 router.route("/signup").post(signup);
 
+router.route("/saveMessage").post(saveMessage);
+router.route("/delete_saved_message").delete(deleteSavedMessage);
 router.param("userId", findUser);
 router.route("/get_by_Id/:userId").get(getById);
 router.route("/update/:userId").put(updateUserDetails);
