15ae6b8cb6a0901a8363ebe076154d52108f6cec
mohit kushwaha <kushwahamohit000@gmail.com>
2021-09-10 18:51:41 +0530

    feat: fetch members route

--- routers/group.router.js
+++ routers/group.router.js
@@ -5,9 +5,11 @@ const {
   addMember,
   removeMember,
   deleteGroup,
+  fetchMembers,
   updateGroup,
 } = require("../controllers/group.controller");
 
+router.route("/members/:groupId").get(fetchMembers);
 router.route("/create").post(createGroup);
 router.route("/add_member").post(addMember);
 router.route("/update_group").put(updateGroup);
